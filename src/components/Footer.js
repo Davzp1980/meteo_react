1c0b5bb3b3deb90fbbe0eac876e7a147d948fcb2
Dav1980 <davzp1980@gmail.com>
2023-12-23 22:57:50 +0200

    Making mobile version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,9 @@ function Footer() {
                 <div className="footer-links">
                     <ul>
                         <li className="back-front"><span>Back-End:</span></li>
-                        <li><a className="back-link" href="https://github.com/Davzp1980/meteo.git" target="_blank" rel="noopener noreferrer"><img src={require("./img/github.png")} alt="git-logo" width="30"/></a></li>
+                        <li><a className="back-link" href="https://github.com/Davzp1980/meteo.git" target="_blank" rel="noopener noreferrer"><img className="footer-link-img" src={require("./img/github.png")} alt="git-logo" width="30"/></a></li>
                         <li className="back-front"><span>Front-End:</span></li>
-                        <li><a href="https://github.com/Davzp1980/meteo_react.git" target="_blank" rel="noopener noreferrer"><img src={require("./img/github.png")} alt="git-logo" width="30"/></a></li>
+                        <li><a className="front-link" href="https://github.com/Davzp1980/meteo_react.git" target="_blank" rel="noopener noreferrer"><img className="footer-link-img" src={require("./img/github.png")} alt="git-logo" width="30"/></a></li>
                     </ul>  
                 </div>
             </div> 
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
         
         <nav className="header">
              <ul>
-                <li className="text-header"><h1><a href="/"><span className="meteo">Meteo</span ><span className="Data">Data</span></a></h1></li>
+                <li className="text-header"><a href="/"><span className="meteo">Meteo</span ><span className="Data">Data</span></a></li>
                 <li className="home-link"><Link to="/"><img src={require('./img/home.png')} alt="Home" width="30" /></Link></li>
                 <li className="sign-In"><Link  to="/sign-in"><img src={require("./img/sign-in.png")} alt="Sign-In" width="30"/></Link></li>
                 <li className="logout"><Link  to="/logout"><img src={require("./img/logout.png")} alt="Logout" width="30"/></Link></li>
